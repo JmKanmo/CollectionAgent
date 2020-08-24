9b737f4f943167fe9f9f752752c0c0ce441353ed
JunmoKang <nebi25@ex-em.com>
2020-08-24 18:59:58 +0900

    08-24

--- src/test/java/collector/type/HeapMemoryCollectorTest.java
+++ src/test/java/collector/type/HeapMemoryCollectorTest.java
@@ -1,31 +1,5 @@
-package collector.type;
+import static org.junit.jupiter.api.Assertions.*;
 
-import junit.framework.TestCase;
-import org.junit.Test;
+class HeapMemoryCollectorTest {
 
-public class HeapMemoryCollectorTest extends TestCase {
-    @Test
-    public void testThread() {
-        Thread thread = new Thread(new HeapMemoryCollector());
-        thread.start();
-        assertEquals(thread.getState(), Thread.State.RUNNABLE);
-        thread.interrupt();
-
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(thread.getState(), Thread.State.TERMINATED);
-    }
-
-    @Test
-    public void testJMX() {
-        HeapMemoryCollector heapMemoryCollector = new HeapMemoryCollector();
-        assertNotNull(heapMemoryCollector.getMemoryMXBean());
-        assertNotNull(heapMemoryCollector.getGarbageCollectorMXBeans());
-        assertNotNull(heapMemoryCollector.getMemoryManagerMXBeans());
-        assertNotNull(heapMemoryCollector.getMemoryPoolMXBeans());
-    }
 }
